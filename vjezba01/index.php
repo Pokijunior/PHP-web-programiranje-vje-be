ea5926bd65b7ce49c3030f7626f9a253019f189a
Pokijunior11 <lovro@binaria.io>
2024-10-17 14:54:26 +0200

    Create index.php

---
+++ vjezba01/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.">
+    <meta name="keywords" content="Da Vincijev kod, Dan Brown, kriminalistički triler">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">Pročitajte više o Da Vincijevom kodu</a>
+    </p>
+</body>
+</html>
+
+<!-- index.html -->
